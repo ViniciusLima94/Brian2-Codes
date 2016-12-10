db13f53f30591bae0b52a8f120452f1ae161591f
Vinícius Lima <vinicius.lima.cordeiro@usp.br>
2016-12-09 23:45:41 -0200

    Update gl_fi_neuron.py
    Add function phi_v

--- gl_fi_neuron.py
+++ gl_fi_neuron.py
@@ -5,6 +5,27 @@ import matplotlib
 
 defaultclock.dt = 1.0*ms
 
+@implementation('cpp', '''
+     #include <math.h>
+     double phi_v(double v, double vt, double vs, double vr, double gamma, double r, double delta) {
+        if(v <= vt) {
+        	return 0;
+        } else if(v > vt && v <= vs) {
+        	return pow(gamma*(v-vt), r) + delta;
+        } else if(v > vs) {
+        	return 1;
+        }
+     }
+     ''')
+@check_units(v=volt, vt=volt, vs=volt, vr=volt, gamma=1/volt,r=1,delta=1, result=1)
+def phi_v(v,vt, vs, vr, gamma,r,delta):
+	if v <= vt:
+		return 0
+	elif v > vt and v<=vs:
+		return (gamma*(v-vt))**r + delta
+	elif v > vs:
+		return 1
+
 el_class = 'RS';  # Electrophysiologic class (RS or FS)
 
 if el_class == 'RS':
@@ -36,7 +57,7 @@ eqs = '''
 	I : amp
 '''
 
-n = NeuronGroup(num, eqs, threshold='(v > vt and v<=vs and rand()<(gamma*(v-vt))**r+delta) or v > vs', reset='v=vr', method='euler', refractory=0.0*ms)
+n = NeuronGroup(num, eqs, threshold='rand() < phi_v(v,vt, vs, vr, gamma,r,delta)', reset='v=vr', method='euler', refractory=0.0*ms)
 n.v = vr
 n.I = '300*pA * i / (num-1)'
 
@@ -47,3 +68,5 @@ plot(n.I/pA, monitor.count / tsim)
 xlabel('I (pA)')
 ylabel('Firing rate (Hz)')
 show()
+
+
