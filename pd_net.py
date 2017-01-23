5b4eeb8be315dac9762f7617aabaede8db02dc68
shimoura <renanshimoura@gmail.com>
2017-01-23 18:59:48 -0200

    Thalamic input added
    You can choose the thalamic stimulation time by changing the duration in net.run(..). Basically, this was made by adding or removing the object "thal_con" which is responsible to store the thalamic connections.

--- pd_net.py
+++ pd_net.py
@@ -1,14 +1,15 @@
 # Potjans & Diesmann cortical microcircuit model implemented on Brian2.
 
+import matplotlib
+#matplotlib.use('Agg')
+
 from brian2 import *
 import numpy as np
 #set_device('genn')
 
-matplotlib.use('Agg')
-
 # Seting the simulation to run with openmp
-set_device('cpp_standalone', directory='PD')
-prefs.devices.cpp_standalone.openmp_threads = 4
+#set_device('cpp_standalone', directory='PD')
+#prefs.devices.cpp_standalone.openmp_threads = 10
 
 ###############################################################################
 # Simulation parameters
@@ -27,22 +28,24 @@ frac = [.2680, .0756, .2840, .0710, .0628, .0138, .1866, .0382 ]
 #Number of neurons per layer
 n_layer = [N*f for f in frac]
 n_layer = [int(round(n_pop)) for n_pop in n_layer]
+
 # Reescale factor
 rf = 80000.0 / N
 nn_cum = cumsum(n_layer)
+
 # Background number per layer
 bg_layer = [1600, 1500 ,2100, 1900, 2000, 1900, 2900, 1850]
 bg_layer = [bg_pop/rf for bg_pop in bg_layer]   # Reescaling
-# Prob. connection table
 
-table = [[0.1009,  0.1689, 0.0437, 0.0818, 0.0323, 0.,     0.0076, 0.    ],
-        [0.1346,   0.1371, 0.0316, 0.0515, 0.0755, 0.,     0.0042, 0.    ],
-        [0.0077,   0.0059, 0.0497, 0.135,  0.0067, 0.0003, 0.0453, 0.    ],
-        [0.0691,   0.0029, 0.0794, 0.1597, 0.0033, 0.,     0.1057, 0.    ],
-        [0.1004,   0.0622, 0.0505, 0.0057, 0.0831, 0.3726, 0.0204, 0.    ],
-        [0.0548,   0.0269, 0.0257, 0.0022, 0.06,   0.3158, 0.0086, 0.    ],
-        [0.0156,   0.0066, 0.0211, 0.0166, 0.0572, 0.0197, 0.0396, 0.2252],
-        [0.0364,   0.001,  0.0034, 0.0005, 0.0277, 0.008,  0.0658, 0.144 ]]
+# Prob. connection table
+table = [[0.1009,  0.1689, 0.0437, 0.0818, 0.0323, 0.,     0.0076, 0.,     0.    ],
+        [0.1346,   0.1371, 0.0316, 0.0515, 0.0755, 0.,     0.0042, 0.,     0.    ],
+        [0.0077,   0.0059, 0.0497, 0.135,  0.0067, 0.0003, 0.0453, 0.,     0.0983],
+        [0.0691,   0.0029, 0.0794, 0.1597, 0.0033, 0.,     0.1057, 0.,     0.0619],
+        [0.1004,   0.0622, 0.0505, 0.0057, 0.0831, 0.3726, 0.0204, 0.,     0.    ],
+        [0.0548,   0.0269, 0.0257, 0.0022, 0.06,   0.3158, 0.0086, 0.,     0.    ],
+        [0.0156,   0.0066, 0.0211, 0.0166, 0.0572, 0.0197, 0.0396, 0.2252, 0.0512],
+        [0.0364,   0.001,  0.0034, 0.0005, 0.0277, 0.008,  0.0658, 0.144,  0.0196]]
 
 ###############################################################################
 # Synapses parameters
@@ -52,9 +55,13 @@ std_d_ex = 0.75*ms # Std. Excitatory delay
 d_in = 0.75*ms      # Inhibitory delay
 std_d_in = 0.4*ms  # Std. Inhibitory delay
 
-w_ex = rf*.35*mV      # Excitatory weight
-std_w_ex = rf*.0352*mV  # Standard deviation weigth
+w_ex = rf*87.8*pA       # Excitatory weight
+std_w_ex = rf*8.8*pA    # Standard deviation weigth
 g = 4.0                 # Inhibitory weight balance
+tau_syn = 0.8*ms        # Post-synaptic current time constant
+
+w_thal = 2*rf*87.8*pA       # Excitatory weight
+
 
 ###############################################################################
 # Neuron model parameters
@@ -64,20 +71,19 @@ v0 = -58*mV         # Initial membrane potential
 std_v0 = 10*mV      # Standard deviation for initial values of v0
 tau_m   = 10.0*ms   # Membrane time constant
 tau_ref = 2.0*ms    # Absolute refractory period
-tau_syn = 0.5*ms    # Post-synaptic current time constant
-R       = 40*Mohm   # Membrane capacity
+Cm      = 250*pF   # Membrane capacity
 v_r     = -65*mV    # Reset potential
 v_th    = -50*mV    # Threshold potential
 
 eqs = '''
-	dv/dt = ( -v + v_r + R*I ) / tau_m : volt (unless refractory)
-	I : amp
+	dv/dt = ((-v + v_r)/tau_m + I/Cm) : volt (unless refractory)
+    I : amp
 '''
 
 ###############################################################################
 # Creating neurons
 ###############################################################################
-neurons = NeuronGroup(sum(n_layer), eqs, threshold='v>v_th', reset='v=v_r', method='linear', refractory=tau_ref)
+neurons = NeuronGroup(sum(n_layer), eqs, threshold='v>v_th', reset='v=v_r', method='euler', refractory=tau_ref)
 neurons.v = 'v0 + std_v0*randn()'
 neurons.I = 0.0*pA
 
@@ -98,17 +104,17 @@ for c in range(0, 8):
         # Excitatory layer
         if (c % 2) == 0:
             if c == 2 and r == 0:
-                con.append( Synapses(p[c],p[r], """ w:volt """,on_pre = 'v_post += 2*w') )
+                con.append( Synapses(p[c],p[r], """ w:amp """,on_pre = 'v_post += 2*(w/Cm)*1*ms') )
                 con[-1].connect(condition='i!=j', p=table[r][c])
                 con[-1].w = 'clip((w_ex + std_w_ex*randn()),w_ex*0.1, w_ex*10.0)'
             else:
-                con.append( Synapses(p[c],p[r], """ w:volt """,on_pre = 'v_post += w') )
+                con.append( Synapses(p[c],p[r], """ w:amp """,on_pre = 'v_post += (w/Cm)*1*ms') )
                 con[-1].connect(condition='i!=j', p=table[r][c])
                 con[-1].w = 'clip((w_ex + std_w_ex*randn()),w_ex*0.1, w_ex*10.0)'
             con[-1].delay = 'clip(d_ex + std_d_ex*randn(),0,d_ex*10)'
         # Inhibitory layer
         else:
-            con.append( Synapses(p[c],p[r], """ w:volt """,on_pre = 'v_post -= g*w') )
+            con.append( Synapses(p[c],p[r], """ w:amp """,on_pre = 'v_post -= g*(w/Cm)*1*ms') )
             con[-1].connect(condition='i!=j', p=table[r][c])
             con[-1].w = 'clip((w_ex + std_w_ex*randn()),w_ex*0.1, w_ex*10.0)'
             con[-1].delay = 'clip(d_in + std_d_in*randn(),0,d_in*10)'
@@ -118,7 +124,17 @@ for c in range(0, 8):
 ###############################################################################
 bg_in  = []
 for r in range(0, 8):
-	bg_in.append( PoissonInput(p[r], 'v', bg_layer[r], 8*Hz, weight = w_ex ) )
+	bg_in.append( PoissonInput(p[r], 'v', bg_layer[r], 8*Hz, weight = (w_ex/Cm)*1*ms ) )
+
+###############################################################################
+# Creating thalamic neurons as poissonian inputs
+###############################################################################
+thal_input = PoissonGroup(902/rf, rates=15*Hz)
+thal_con = []
+for r in range(0,8):
+    thal_con.append(Synapses(thal_input,p[r],""" w:amp """,on_pre = 'v_post += (w/Cm)*1*ms'))
+    thal_con[-1].connect(condition='i!=j', p=table[r][8])
+    thal_con[-1].w = 'clip((w_thal + std_w_ex*randn()),w_ex*0.1, w_ex*10.0)'
 
 ###############################################################################
 # Creating spike monitors
@@ -146,7 +162,15 @@ f_6i  = spikemon[7].count;
 ###############################################################################
 net = Network(collect())
 net.add(neurons,p, con, spikemon, bg_in)    # Adding objects to the simulation
-net.run(tsim,report='stdout')
+net.run(0.2*second,report='stdout')
+
+# Adding thalamic input
+net.add(thal_con)
+net.run(0.5*second, report='stdout')
+
+# Removing thalamic input
+net.remove(thal_con)
+net.run(0.3*second, report='stdout')
 
 ###############################################################################
 # Raster plot
@@ -177,3 +201,4 @@ freqs.append(mean(f_6i)/tsim)
 ind = np.arange(8)
 bar(ind, freqs)
 savefig('freqs_PDnet.png', dpi=300)
+close()
